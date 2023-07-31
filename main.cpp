d45f1c41451e73e4bfbd6a18fa3de5766d2e81ab
Frank Boerman <frank@fboerman.nl>
2023-07-31 22:57:05 +0200

    added day2 part 1

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <arrow/api.h>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
 int day1() {
     cout << "day1" << endl;
-    ifstream stream("/home/frank/CLionProjects/adventofcode/input_files/day1.txt");
+    ifstream stream("/home/frank/CLionProjects/adventofcodecpp/input_files/day1.txt");
     string line;
     vector<int> elves = {};
 
@@ -39,7 +40,49 @@ int day1() {
     return 0;
 }
 
+int day2() {
+    cout << "day1" << endl;
+    ifstream stream("/home/frank/CLionProjects/adventofcodecpp/input_files/day2.txt");
+    string line;
+
+    int score = 0;
+    while(getline(stream, line)) {
+        istringstream iss(line);
+        char opp_c, me_c;
+        iss >> opp_c >> me_c;
+
+        int me = int(me_c) - 88;
+        int opp = int(opp_c) - 65;
+
+        //vector of winning elements, each element wins of the element next to it
+        // CBA
+        // formula: f(i) -> (i%n+n)%n
+        // A -> i=2, B -> i=1, C -> i=0 then if f(i+1) == opp then win, otherwise loss
+        // or turn around ABC with mapping A->0, B -> 1, C->2 then f(i-1) == opp then win otherwise loss
+        // in which 0 is rock 1 is paper 2 is scissors
+
+        int i = me-1;
+        int wins_from = (i%3+3)%3;
+        // score for game itself
+        if(opp == wins_from){
+            //win!
+            score += 6;
+        } else if (me == opp) {
+            //draw
+            score += 3;
+        }
+
+        // score for playing
+        score += me + 1;
+
+    }
+
+    cout << "Score: " << score << endl;
+
+    return 0;
+}
+
 
 int main() {
-    return day1();
+    return day2();
 }
