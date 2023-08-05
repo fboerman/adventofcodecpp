e4a1961de4978d548022500b1c9f22157106c1e5
Frank Boerman <frank@fboerman.nl>
2023-08-05 22:36:48 +0200

    added day3 part2

--- main.cpp
+++ main.cpp
@@ -106,8 +106,18 @@ int day2() {
 
 int day3() {
     cout << "day3" << endl;
-    ifstream stream("/home/frank/CLionProjects/adventofcodecpp/input_files/day3.txt");
+    string fname = "/home/frank/CLionProjects/adventofcode/input_files/day3.txt";
+    ifstream stream(fname);
     string line;
+
+    auto f_priority = [](char item){
+        int item_i = int(item);
+        if((item_i >= 97) && (item_i <= 122)) {
+            return item_i - 96;
+        } else {
+            return item_i - 38;
+        }
+    };
 
     int sum_priorities = 0;
     while(getline(stream, line)) {
@@ -119,18 +129,44 @@ int day3() {
         set_intersection(rucksack1.begin(), rucksack1.end(),
                          rucksack2.begin(), rucksack2.end(),
                          back_inserter(intersections));
-
-        int prio = int(intersections[0]);
-        if((prio >= 97) && (prio <= 122)) {
-            // lower case
-            sum_priorities += prio - 96;
-        } else {
-            // upper case
-            sum_priorities += prio - 38;
-        }
+        sum_priorities += f_priority(intersections[0]);
     }
 
     cout << "sum of priorities of double items is " << sum_priorities << endl;
+
+    // for part 2 we gonna rescan the whole file because the approach is different
+    stream.close();
+    stream.open(fname);
+
+    bool stop = false;
+    sum_priorities = 0;
+    while(true) {
+        // groups of three
+        vector<vector<char>> rucksacks{};
+        for(int i=0;i<3;i++){
+            if(!getline(stream, line)){
+                stop = true;
+                break;
+            }
+            vector<char> V(line.begin(), line.end());
+            sort(V.begin(), V.end());
+            rucksacks.push_back(V);
+        }
+        if(stop){
+            break;
+        }
+        vector<char> intersections, intersections2;
+        set_intersection(rucksacks[0].begin(), rucksacks[0].end(),
+                         rucksacks[1].begin(), rucksacks[1].end(),
+                         back_inserter(intersections));
+        sort(intersections.begin(), intersections.end());
+        set_intersection(intersections.begin(), intersections.end(),
+                         rucksacks[2].begin(), rucksacks[2].end(),
+                         back_inserter(intersections2));
+        sum_priorities += f_priority(intersections2[0]);
+    }
+
+    cout << "sum of priorities for badges is " << sum_priorities << endl;
 
     return 0;
 }
